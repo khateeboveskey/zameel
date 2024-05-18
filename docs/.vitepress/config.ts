1fe2155414eb8c1cd2f7b88ce5c9c49ba60eab5d
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-05-18 18:25:24 +0300

    build: add vitepress as a development dependency

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Zameel Docs",
+  description: "Technical docs for Zameel, your never-be-absent classmate.",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Getting Started',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'Contribution', link: '/CONTRIBUTING' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/khateeboveskey/zameel' }
+    ]
+  },
+  base: '/zameel/'
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Zameel"
+  text: "Documentation"
+  tagline: Your never-be-absent classmate
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /introduction
+    - theme: alt
+      text: Contributing Guide
+      link: /CONTRIBUTING
+
+features:
+  - title: Open Source
+    icon: 🔓
+    details: Fully open source, you can use it, improve it, or make your own from it.
+  - title: Cross Platform
+    icon: 📱
+    details: Available for Android and iOS with the power of Capacitor.
+  - title: User-centered
+    icon: 👩🏻‍🎓
+    details: Made by students, for students, solving their studying-related problems!
+---
